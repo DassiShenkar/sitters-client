410ff33cd36bd39de3860cf45990d71cc26acce6
Arel Gindos <rlgindos@gmail.com>
2017-03-05 04:30:46 +0200

    Added facebook login with all parameters

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import React, { PropTypes, Component } from 'react';
-//import Login from "./loginWithFB";
-//import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
 class App extends Component {
 
     constructor() {
         super();
-
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=268453370262293";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
     }
 
     update(e) {
         this.setState({txt: e.target.value});
     }
 
+
     render() {
         // let txt = this.props.txt;
         return (
             <div>
-                <div id="fb-root"></div>
-                <div className="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false"></div>
+
             </div>
             // <div className="App">
             //     <div className="App-header">
