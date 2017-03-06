f37a478ac08ec72cbe309e3e3f7e6fbf0b8bf836
Arel Gindos <rlgindos@gmail.com>
2017-03-06 21:01:08 +0200

    Started implement sitter create

--- src/components/Register.js
+++ src/components/Register.js
@@ -15,7 +15,8 @@ class Register extends Component {
         super();
         this.handleSubmitParent = this.handleSubmitParent.bind(this);
         this.handleSubmitSitter = this.handleSubmitSitter.bind(this);
-        this.handleSelectChange          = this.handleSelectChange.bind(this);
+        this.handleSelectChange = this.handleSelectChange.bind(this);
+        this.onChangeImmediate = this.onChangeImmediate.bind(this);
         let usr = JSON.parse(localStorage.getItem('user'));
         console.log(usr);
         if(usr.birthday){ // calculate age from birthday
@@ -26,6 +27,7 @@ class Register extends Component {
         this.state = {
             selectedForm: "parent",
             genderFilter: usr.gender?usr.gender:"female",
+            immediateFilter: "false",
             name: usr.name,
             email: usr.email,
             gender: usr.gender,
@@ -75,14 +77,23 @@ class Register extends Component {
         }
     }
 
-    onChangeHours(hours) {
-        if (hours === "Mornings")
-            this.setState({workingHours: "Mornings"});
-        else if (hours === "Evenings")
-            this.setState({workingHours: "Evenings"});
-        else
-            this.setState({workingHours: "All day"});
+    onChangeImmediate (immediate) {
+        if (immediate === "false") {
+            this.setState({immediateFilter: "false"});
+        }
+        else {
+            this.setState({immediateFilter: "true"});
+        }
     }
+    //
+    // onChangeHours(hours) {
+    //     if (hours === "Mornings")
+    //         this.setState({workingHours: "Mornings"});
+    //     else if (hours === "Evenings")
+    //         this.setState({workingHours: "Evenings"});
+    //     else
+    //         this.setState({workingHours: "All day"});
+    // }
 
     handleSubmitParent(e) {
         e.preventDefault();
@@ -127,8 +138,47 @@ class Register extends Component {
     };
     handleSubmitSitter(e) {// pass education
         e.preventDefault();
-    }
+        console.log(this.refs);
+        let sitter = {
+            name: this.refs.name.defaultValue,
+            email: this.refs.email.defaultValue,
+            gender: this.state.genderFilter,
+            age: this.refs.age.defaultValue,
+            address: {
+                city: this.refs.city.value,
+                street: this.refs.street.value,
+                houseNumber: this.refs.houseNumber.value
+            },
+            profilePicture: this.state.profilePicture,
+            coverPhoto: this.state.coverPhoto,
+            timezone: this.state.timezone,
+            languages: this.state.languages,
+            education: this.state.education,
+            minAge: this.refs.minAge.value,
+            maxAge: this.refs.maxAge.value,
+            hourFee: this.refs.hourFee.value,
 
+
+
+        };
+        console.log('sitter');
+        console.log(sitter);
+
+        $.ajax({
+            url: 'localhost:4000/sitter/create',
+            dataType: 'json',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify(sitter),
+            success: function (data) {
+                console.log(data);
+                location.replace("feed");
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    }
 
     render(){
 
@@ -193,6 +243,8 @@ class Register extends Component {
                             onChange={this.handleSelectChange.bind(this)}
                             placeholder="Select your favourite(s)"
                         />
+
+                        <h4>Child</h4>
                         <li>
                             <label className="login-label" htmlFor="maxPrice">Max price for watch</label>
                             <input type="number" name="maxPrice" defaultValue="" id="maxPrice" ref="maxPrice" placeholder="20" min="0" max="100"/>
@@ -207,6 +259,9 @@ class Register extends Component {
                             <input className="input age" id="age" name="prof1" type="number" placeholder="3"
                                    ref="childAge" required min="0" max="12"/>
                         </li>
+                        <h4>Expertise</h4>{/*TODO: implement multi checkbox*/}
+                        <h4>Hobbies</h4>{/*TODO: implement multi checkbox*/}
+                        <h4>Special needs</h4>{/*TODO: implement multi checkbox*/}
                     </ul>
                     <input type="submit" className="submit-invite" value="Sign Up"/>
                 </form>;
@@ -216,32 +271,30 @@ class Register extends Component {
                 <form id="register-form" onSubmit={this.handleSubmitSitter}>
                     <ul className="login-input-fields">
                         <li>
-                            <label htmlFor="city" className="login-label" >City</label>
-                            <input type="text" id="city" ref="city" required="true" placeholder="Tel Aviv"/>
+                            <label className="login-label" htmlFor="name">Name</label>
+                            <input type="text" name="name" id="name" ref="name" defaultValue={this.state.name} placeholder="Moshe Levi"/>
                         </li>
                         <li>
-                            <label htmlFor="street" className="login-label" >Street</label>
-                            <input type="text" id="street" ref="street" required="true" placeholder="Arlozorov"/>
+                            <label className="login-label" htmlFor="email">Email</label>
+                            <input type="email" name="email" id="email" ref="email" defaultValue={this.state.email} placeholder="example@gmail.com"/>
                         </li>
                         <li>
-                            <label htmlFor="houseNumber" className="login-label" >House Number</label>
-                            <input type="number" id="houseNumber" ref="houseNumber" required="true" min="0" max="350"
-                                   placeholder="56"/>
+                            <label className="login-label" htmlFor="age">Age</label>
+                            <input type="number" min="0" max="120" name="age" id="age" ref="age" defaultValue={this.state.age} />
+                        </li>
+                        <h4>Address</h4>
+                        <li>
+                            <label className="login-label" htmlFor="city">City</label>
+                            <input type="text" id="city" ref="city" required defaultValue="" placeholder="Tel Aviv"/>
                         </li>
                         <li>
-                            <label htmlFor="min-age" className="login-label" >Minimum Age</label>
-                            <input className="input name" id="min-age" type="number" placeholder="1" ref="minAge"
-                                   required="true" min="0" max="12"/>
+                            <label className="login-label" htmlFor="street">Street</label>
+                            <input type="text" id="street" ref="street" required defaultValue="" placeholder="Arlozorov"/>
                         </li>
                         <li>
-                            <label htmlFor="max-age" className="login-label" >Maximum Age</label>
-                            <input className="input age" id="max-age" type="number" placeholder="4" ref="maxAge"
-                                   required="true" min="0" max="12"/>
-                        </li>
-                        <li>
-                            <label htmlFor="hour-fee" className="login-label" >Hour Fee</label>
-                            <input className="input age" id="hour-fee" type="number" placeholder="25$" ref="hourFee"
-                                   required="true" min="1" max="400"/>
+                            <label className="login-label" htmlFor="houseNumber">House Number</label>
+                            <input type="number" id="houseNumber" ref="houseNumber" defaultValue="" required min="0" max="350"
+                                   placeholder="36"/>
                         </li>
                         <h3>Gender</h3>
                         <ul className="user-select">
@@ -257,27 +310,55 @@ class Register extends Component {
                                        onChange={this.onChangeGender.bind(this, "female")}/>
                             </li>
                         </ul>
-                        <h3>Working Hours</h3>
+                        <h4>Profile picture</h4>
+                        <li>
+                            <div>
+                                <img src={this.state.profilePicture} alt="Profile picture"/>
+                            </div>
+                        </li>
+                        <h4>Languages</h4>
+                        <Select
+                            name="form-field-name"
+                            multi={true}
+                            value={this.state.languages}
+                            options={this.state.options}
+                            onChange={this.handleSelectChange.bind(this)}
+                            placeholder="Select your favourite(s)"
+                        />
+                        <li>
+                            <label className="login-label" htmlFor="age">Minimum age to save children</label>
+                            <input type="number" min="0" max="12" name="minAge" id="minAge" ref="minAge" placeholder="0"/>
+                        </li>
+                        <li>
+                            <label className="login-label" htmlFor="age">Maximum age to save children</label>
+                            <input type="number" min="0" max="12" name="maxAge" id="maxAge" ref="maxAge" placeholder="8"/>
+                        </li>
+                        <li>
+                            <label htmlFor="hour-fee" className="login-label" >Hour Fee</label>
+                            <input className="input age" id="hour-fee" type="number" placeholder="25" ref="hourFee"
+                                   required="true" min="1" max="100"/>
+                        </li>
+
+                        <h4>Working Hours</h4>{/*TODO: think about this component*/}
+
+                        <h3>Immediate availability</h3>
                         <ul className="user-select">
                             <li className="user-option">
-                                <label htmlFor="mornings">Mornings</label>
-                                <input id="mornings" value="mornings" type="radio"
-                                       checked={this.state.workingHours === "Mornings"} name="hours-radio"
-                                       onChange={this.onChangeHours.bind(this, "Mornings")}/>
+                                <label htmlFor="immediate_true">True</label>
+                                <input id="immediate_true" value="immediate_true" type="radio" checked={this.state.immediateFilter === "true"}
+                                       name="immediate-radio" onChange={this.onChangeImmediate.bind(this, "true")}/>
                             </li>
                             <li className="user-option">
-                                <label htmlFor="evenings">Evenings</label>
-                                <input id="evenings" value="evenings" type="radio"
-                                       checked={this.state.workingHours === "Evenings"} name="hours-radio"
-                                       onChange={this.onChangeHours.bind(this, "Evenings")}/>
-                            </li>
-                            <li className="user-option">
-                                <label htmlFor="allDay">All day</label>
-                                <input id="allDay" value="allDay" type="radio"
-                                       checked={this.state.workingHours === "All day"} name="hours-radio"
-                                       onChange={this.onChangeHours.bind(this, "All day")}/>
+                                <label htmlFor="immediate_false">False</label>
+                                <input id="immediate_false" value="immediate_false" type="radio"
+                                       checked={this.state.immediateFilter === "false"} name="immediate-radio"
+                                       onChange={this.onChangeImmediate.bind(this, "false")}/>
                             </li>
                         </ul>
+
+
+
+
                     </ul>
                     <input type="submit" className="submit-invite" value="Sign Up"/>
                 </form>;
