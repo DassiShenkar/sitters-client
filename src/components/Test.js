e1921c8abcdad1eb38bc84c65c878b055196e0ca
Arel Gindos <rlgindos@gmail.com>
2017-03-24 16:41:03 +0300

    Added icons and sitter data

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,113 +1,16 @@
 import React from 'react';
-import PersonalityTest from './PersonalityTest'
-import SearchByTab from './SearchByTab'
-//import {Range} from 'rc-slider';
-import 'react-tab-panel/index.css'
-import 'rc-slider/assets/index.css';
-const Slider = require('rc-slider');
-const Range = Slider.Range;
+
+
 class Test extends React.Component {
     constructor(){
         super();
-        this.changeValue = this.changeValue.bind(this);
-        this.state = {
-            min:0,
-            max:100,
-            step:1,
-            currentValue: 50
-        }
     }
-    changeValue(value){
-        console.log(value);
 
-    }
     render(){
-        let questions =[{
-            "question": "I consider myself as an investor in his own field",
-            "category": "characteristics",
-            "method": "normal",
-            "choice": 0
-        },{
-            "question": "I consider myself a responsible person",
-            "category": "characteristics",
-            "method": "normal",
-            "choice": 0
-        },{
-            "question": "I consider myself as having a sensitivity to the needs of others",
-            "category": "characteristics",
-            "method": "normal",
-            "choice": 0
-        },{
-            "question": "I used to invest mainly in the things I'm good, and to give up when I'm having difficulty",
-            "category": "characteristics",
-            "method": "reverse",
-            "choice": 0
-        },{
-            "question": "I consider myself as a creative person who can provide children with an interest",
-            "category": "characteristics",
-            "method": "normal",
-            "choice": 0
-        },{
-            "question": "It is important for me to know everything you expect me to clearly and completely",
-            "category": "expectations",
-            "method": "normal",
-            "choice": 0
-        },{
-            "question": "I consider myself punctual times",
-            "category": "expectations",
-            "method": "normal",
-            "choice": 0
-        },{
-            "question": "I believe I can fulfill the expectations of parents",
-            "category": "expectations",
-            "method": "normal",
-            "choice": 0
-        },{
-            "question": "It is important to me that parents will be satisfied with my work",
-            "category": "expectations",
-            "method": "normal",
-            "choice": 0
-        },{
-            "question": "I expect myself to demonstrate assertiveness in cases where children have demonstrated a lack of discipline",
-            "category": "expectations",
-            "method": "normal",
-            "choice": 0
-        },{
-            "question": "Especially problematic situations of stress I can't handle",
-            "category": "Integrity",
-            "method": "reverse",
-            "choice": 0
-        },{
-            "question": "I think that all people in the world are honest",
-            "category": "Integrity",
-            "method": "special",
-            "choice": 0
-        },{
-            "question": "At least once in life I took an object that belongs to someone without permission",
-            "category": "Integrity",
-            "method": "normal",
-            "choice": 0
-        },{
-            "question": "In the case of an extreme lack of knowledge of how to correctly operate, I will be relying on my intuition and not call to ask the parents.",
-            "category": "Integrity",
-            "method": "reverse",
-            "choice": 0
-        },{
-            "question": "The boy cursed his brother and will be punished if parents know, so you'd rather not tell his parents of child care",
-            "category": "Integrity",
-            "method": "reverse",
-            "choice": 0
-        }];
+
         return (
 
-            <div style
-            >
-                {/*<SearchByTab/>*/}
-                {/*<PersonalityTest questions={questions} />*/}
-                <Range allowCross={false}
-                        min={5}
-                        max={50}
-                />
+            <div>
             </div>
         );
     }
