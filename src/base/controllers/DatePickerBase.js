3de14f91fd81ce0443c724008492f7f9290a45ec
DassiShenkar <dassi.shenkar@gmail.com>
2017-04-11 20:31:27 +0300

    generalizing DatePicker. BUG!!! state has correct value but view always shows default value

--- src/base/controllers/DatePickerBase.js
+++ src/base/controllers/DatePickerBase.js
@@ -5,18 +5,19 @@ class DatePickerBase extends React.Component {
     constructor(props){
         super(props);
         this.handleChange = this.handleChange.bind(this);
-        this.state = {
-            value:  new Date().toISOString()
-        }
+        // this.state = {
+        //     value:  new Date().toISOString()
+        // }
     }
-    handleChange(value, formattedValue){
-        this.setState({
-            value: value, // ISO String, ex: "2016-11-19T12:00:00.000Z"
-            formattedValue: formattedValue, // Formatted String, ex: "11/19/2016"
-            day: dateFormat(value, "dddd") // dddd = the full day of the week
-        },
-            this.props.changeDateTimeValues
-        );
+    handleChange(e){
+        this.props.action(e);
+        // this.setState({
+        //     value: value, // ISO String, ex: "2016-11-19T12:00:00.000Z"
+        //     formattedValue: formattedValue, // Formatted String, ex: "11/19/2016"
+        //     day: dateFormat(value, "dddd") // dddd = the full day of the week
+        // },
+        //     this.props.changeDateTimeValues
+        // );
 
     }
 }
