ed73f9407565f1d3f07e465703bfa8c6ea4532cf
MusicGindos <rlgindos@gmail.com>
2017-04-14 13:55:59 +0300

    Started implement NavBase with redux

--- src/base/NavBase.js
+++ src/base/NavBase.js
@@ -5,22 +5,22 @@ class NavBase extends React.Component {
 
     constructor(props) {
         super(props);
-        this.onClickSearch = this.onClickSearch.bind(this);
-        this.onClickMail = this.onClickMail.bind(this);
-        this.onClickNotification = this.onClickNotification.bind(this);
+        // this.onClickSearch = this.onClickSearch.bind(this);
+        // this.onClickMail = this.onClickMail.bind(this);
+        // this.onClickNotification = this.onClickNotification.bind(this);
     }
 
-    onClickSearch(e) {
-        e.preventDefault();
+    onClick(view) {
+        console.log(view);
     }
 
-    onClickMail(e) {
-        e.preventDefault();
-    }
-
-    onClickNotification(e) {
-        e.preventDefault();
-    }
+    // onClickMail(e) {
+    //     e.preventDefault();
+    // }
+    //
+    // onClickNotification(e) {
+    //     e.preventDefault();
+    // }
 }
 
 export default NavBase;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,29 +3,45 @@ import React from 'react';
 import {Link} from 'react-router';
 
 import Badge from '../../node_modules/react-bootstrap/lib/Badge';
+import {Button} from "react-bootstrap";
 
 //Components
 import NavBase from '../base/NavBase'
-import SearchSVG from '../styles/icons/Search'
+import Search from '../styles/icons/Search'
 import NotificationSVG from '../styles/icons/Notification'
 import MailSVG from '../styles/icons/Mail'
 import DropdownMenu from './DropDownMenu'
 
+
+
 class Nav extends NavBase {
+
     render() {
         return (
             <div id="main-nav">
                 <img src={this.props.image} alt={this.props.alt}/>
                 <p>{ "Hi," + this.props.name}</p>
-                <Link to="/search" onClick={this.onClickSearch}><SearchSVG/></Link>
-                <Link to="/notifications"
-                      onClick={this.onClickMail}><NotificationSVG/>
+                <Button onClick={this.onClick.bind(this, "searchBy")}>
+                    <Search/>
+                </Button>
+                <Button onClick={this.onClick.bind(this, "notifications")}>
+                    <NotificationSVG/>
                     <Badge>{this.props.notifications.filter(notification => !notification.wasRead).length}</Badge>
-                </Link>
-                <Link to="/invites"
-                      onClick={this.onClickNotification}><MailSVG/>
+                </Button>
+                <Button onClick={this.onClick.bind(this, "invites")}>
+                    <MailSVG/>
                     <Badge>{this.props.invites.filter(invite => !invite.wasRead).length}</Badge>
-                </Link>
+                </Button>
+
+                {/*<Link to="/search" onClick={this.onClickSearch}><SearchSVG/></Link>*/}
+                {/*<Link to="/notifications"*/}
+                      {/*onClick={this.onClickMail}><MailSVG/>*/}
+                    {/*<Badge>{this.props.notifications.filter(notification => !notification.wasRead).length}</Badge>*/}
+                {/*</Link>*/}
+                {/*<Link to="/invites"*/}
+                      {/*onClick={this.onClickNotification}><MailSVG/>*/}
+                    {/*<Badge>{this.props.invites.filter(invite => !invite.wasRead).length}</Badge>*/}
+                {/*</Link>*/}
                 <DropdownMenu {...this.props}/>
             </div>
         )
