77e1f5bb2c05ad1513f7ea7125f1a153680a94a0
MusicGindos <rlgindos@gmail.com>
2017-04-15 12:14:02 +0300

    Added sitters images to google maps

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import shouldPureComponentUpdate from 'react-pure-render/function';
 import LocationIcon from '../styles/icons/Location'
+import {Image} from "react-bootstrap";
 
 
 export default class Location extends React.Component {
@@ -15,7 +16,7 @@ export default class Location extends React.Component {
     render() {
         return (
             <div>
-                <LocationIcon/>
+                <Image src={this.props.sitter.profilePicture} width="30px" height="30px" circle={true}/>
             </div>
         );
     }
