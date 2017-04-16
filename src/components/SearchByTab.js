1ddb8dbd9de53ef8f400a2df7db62255f9317fae
DassiShenkar <dassi.shenkar@gmail.com>
2017-04-16 23:31:08 +0300

    searchby design

--- src/components/SearchByTab.js
+++ src/components/SearchByTab.js
@@ -1,37 +1,68 @@
+//external sources
 import React from 'react'
+
+import {Nav, NavItem, PageHeader} from 'react-bootstrap';
+import Range from './RangeSlider'
+import GoogleMaps from './GoogleMaps';
+
+//components
 import Location from '../styles/icons/Location'
 import Clock from '../styles/icons/Clock'
 import Dollar from '../styles/icons/Dollar'
-import Range from './RangeSlider'
-import {Tabs, Tab} from 'react-bootstrap-tabs';
-import GoogleMaps from './GoogleMaps'
-import DatePicker from './controllers/DatePicker'
-import TimeInput from './controllers/TimeInput'
+import DatePicker from './controllers/DatePicker';
+import TimeInput from './controllers/TimeInput';
+
 class SearchByTab extends React.Component {
+
+
+    handleSelect(selectedKey) {
+        this.props.actions.searchByActions.setView(selectedKey);
+    }
+
+
     render() {
-        return (
-            <div>
-                <Tabs>
-                    <Tab label={<Location/>}>
-                        <div style={{width: '400px', height: '400px'}}>
-                         <GoogleMaps sitters={this.props.feed.matches}/>
-                         </div>
-                    </Tab>
-                    <Tab label={<Clock/>}>
-                        <p>Search by Time</p>
-                        <DatePicker defaultValue={this.props.searchBy.isoValue} {...this.props} action={this.props.actions.searchByActions.changeInviteDate} changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
-                        <p>From</p>
-                        <TimeInput defaultValue={this.props.searchBy.fromTime} {...this.props} action={this.props.actions.searchByActions.changeInviteFromTime} changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
-                        <p>To</p>
-                        <TimeInput defaultValue={this.props.searchBy.toTime} {...this.props} action={this.props.actions.searchByActions.changeInviteToTime} changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
-                    </Tab>
-                    <Tab label={<Dollar/>}>
-                        <p>Hour Rare</p>
-                        <Range min={0} max={50} {...this.props} action={this.props.actions.rangeActions.changeRange} changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
-                    </Tab>
-                </Tabs>
-            </div>
-        );
+        let navView = null;
+        if (this.props.searchBy.searchView !== null) {
+            let view = this.props.searchBy.searchView;
+            if (view === "location") {
+                navView = <div style={{width: '400px', height: '400px'}}><GoogleMaps sitters={this.props.feed.matches}/>
+                </div>;
+            }
+            else if (view === "time") {
+                navView = <div>
+                    <p>Search by Time</p>
+                    <DatePicker defaultValue={this.props.searchBy.isoValue} {...this.props}
+                                action={this.props.actions.searchByActions.changeInviteDate}
+                                changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
+                    <p>From</p>
+                    <TimeInput defaultValue={this.props.searchBy.fromTime} {...this.props}
+                               action={this.props.actions.searchByActions.changeInviteFromTime}
+                               changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
+                    <p>To</p>
+                    <TimeInput defaultValue={this.props.searchBy.toTime} {...this.props}
+                               action={this.props.actions.searchByActions.changeInviteToTime}
+                               changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
+                </div>;
+            }
+            else if (view === "rate") {
+                navView = <div>
+                    <p>Hour Rate</p>
+                    <Range min={0} max={50} {...this.props} action={this.props.actions.rangeActions.changeRange}
+                           changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
+                </div>
+            }
+            return (
+                <div id="search-panel">
+                    <PageHeader>Quick Search</PageHeader>
+                    <Nav justified onSelect={this.handleSelect.bind(this)}>
+                        <NavItem eventKey="location" title="location"><Location/></NavItem>
+                        <NavItem eventKey="time"><Clock/></NavItem>
+                        <NavItem eventKey="rate"><Dollar/></NavItem>
+                    </Nav>
+                    {navView}
+                </div>
+            );
+        }
     }
 }
 
--- src/components/sitterList/index.js
+++ src/components/sitterList/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-import {Image} from 'react-bootstrap/lib';
+import {Image} from 'react-bootstrap';
 
 import SitterListBase from '../../base/SitterListBase';
 
@@ -17,15 +17,18 @@ class SitterList extends SitterListBase {
             backgroundImage: 'url(' + coverPhoto + ')'
         };
         return (
-            <Link className="match" style={style}
-                  to={this.props.sitters.length > 0 ? '/sitter/' + this.props.sitters[sitterIndex]._id : '#'}>
-                <div className="sitter-info">
-                    <h1 className="matchScore">{this.props.sitters.length > 0 ? this.props.sitters[sitterIndex].matchScore + '% Match!' : 'no matches found'}</h1>
-                    <Image className="profilePic" src={this.props.sitters.length > 0 ? this.props.sitters[sitterIndex].profilePicture : ''}
-                           alt={this.props.sitters.length > 0 ? this.props.sitters[sitterIndex].name : ''} circle/>
-                    <h3 className="sitterName">{this.props.sitters.length > 0 ? this.props.sitters[sitterIndex].name : ''}</h3>
-                </div>
-            </Link>
+            <div>
+                <Link className="match" style={style}
+                      to={this.props.sitters.length > 0 ? '/sitter/' + this.props.sitters[sitterIndex]._id : '#'}>
+                    <div className="sitter-info">
+                        <h1 className="matchScore">{this.props.sitters.length > 0 ? this.props.sitters[sitterIndex].matchScore + '% Match!' : 'no matches found'}</h1>
+                        <Image className="profilePic"
+                               src={this.props.sitters.length > 0 ? this.props.sitters[sitterIndex].profilePicture : ''}
+                               alt={this.props.sitters.length > 0 ? this.props.sitters[sitterIndex].name : ''} circle/>
+                        <h3 className="sitterName">{this.props.sitters.length > 0 ? this.props.sitters[sitterIndex].name : ''}</h3>
+                    </div>
+                </Link>
+            </div>
         )
     }
 }
