d5c568434ff65461f3039f2b2bb5b8a9213870e1
MusicGindos <rlgindos@gmail.com>
2017-04-25 21:46:00 +0300

    deploy to heroku

---
+++ src/components/Test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Test extends React.Component {
+    render() {
+        return(
+            <div className="single-photo">I'm the single grid</div>
+        );
+    }
+}
+
+export default Test;
