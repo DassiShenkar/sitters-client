de7114f63b151451186bf501d0d23f4d719a89c0
MusicGindos <rlgindos@gmail.com>
2017-05-23 23:03:27 +0300

    Added calander

--- src/components/pages/feed/sitterFeed/index.js
+++ src/components/pages/feed/sitterFeed/index.js
@@ -3,45 +3,80 @@ import React from 'react';
 import axios from 'axios';
 import InviteList from '../../../inviteList'
 import strings from "../../../../static/strings";
-
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
 //components
 
 //style
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
+// BigCalendar.setLocalizer(
+//     BigCalendar.momentLocalizer(moment)
+// );
 
+BigCalendar.momentLocalizer(moment);
 class SitterFeed extends React.Component {
     componentWillMount() {
-            let self = this;
-            const userId = document.cookie.replace(/(?:(?:^|.*;\s*)auth_token\s*=\s*([^;]*).*$)|^.*$/, "$1");
-            if (!userId) {
-            } else {
-                axios({
-                    method: 'post',
-                    url: (strings.DEBUG ? strings.LOCALHOST : strings.WEBSITE ) + 'sitter/get',
-                    headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
-                    data: {_id: userId}
+        let self = this;
+        const userId = document.cookie.replace(/(?:(?:^|.*;\s*)auth_token\s*=\s*([^;]*).*$)|^.*$/, "$1");
+        if (!userId) {
+        } else {
+            axios({
+                method: 'post',
+                url: (strings.DEBUG ? strings.LOCALHOST : strings.WEBSITE ) + 'sitter/get',
+                headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
+                data: {_id: userId}
+            })
+                .then(function (sitter) {
+                    if (sitter.data) {  // user exists
+                        self.props.actions.settingsActions.setNotifications(sitter.data.settings.allowNotification);
+                        self.props.actions.settingsActions.setSuggestions(sitter.data.settings.allowSuggestions);
+                        self.props.actions.settingsActions.setShowOnSearch(sitter.data.settings.allowShowOnSearch);
+                        self.props.actions.actionCreators.setSitterData(sitter.data);
+                    }
+                    else { // user not exist
+                        self.props.router.push('/login');
+                    }
                 })
-                    .then(function (sitter) {
-                        if (sitter.data) {  // user exists
-                            self.props.actions.settingsActions.setNotifications(sitter.data.settings.allowNotification);
-                            self.props.actions.settingsActions.setSuggestions(sitter.data.settings.allowSuggestions);
-                            self.props.actions.settingsActions.setShowOnSearch(sitter.data.settings.allowShowOnSearch);
-                            self.props.actions.actionCreators.setSitterData(sitter.data);
-                        }
-                        else { // user not exist
-                            self.props.router.push('/login');
-                        }
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    });
-            }
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
 
     }
     render() {
+        const events = [];
+        this.props.user.invites.forEach(function(invite){
+            events.push({
+                title: 'Watch ' + invite.childName + ' - 20$',
+                // allDay: true,
+                startDate: new Date(invite.startTime),
+                endDate: new Date(invite.endTime)
+            })
+        });
+        // const events = [
+        //     {
+        //         'title': 'All Day Event',
+        //         'allDay': true,
+        //         'startDate': new Date(2017, 4, 24),
+        //         'endDate': new Date(2017, 4, 25)
+        //     },
+        //     {
+        //         'title': 'Long Event',
+        //         'startDate': new Date(2017, 4, 26),
+        //         'endDate': new Date(2017, 4, 29)
+        //     }];
+
         return (
             <div id="sitter-feed">
-                <InviteList isParent={this.props.user.isParent} invites={this.props.user.invites}/>
+                {/*<InviteList isParent={this.props.user.isParent} invites={this.props.user.invites}/>*/}
+                <BigCalendar
+                    events={events}
+                    defaultDate={new Date()}
+                    startAccessor='startDate'
+                    endAccessor='endDate'
+                    {... this.props}
+                />
             </div>
         );
     }
