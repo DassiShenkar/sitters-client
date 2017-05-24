02911750411ff32398a257891227cac8287bc05f
MusicGindos <rlgindos@gmail.com>
2017-05-24 20:15:43 +0300

    Added available now

--- src/components/panels/searchPanel/index.js
+++ src/components/panels/searchPanel/index.js
@@ -29,42 +29,48 @@ class SearchByTab extends React.Component {
         let navView = null;
         if (this.props.searchBy.searchView !== null) {
             let view = this.props.searchBy.searchView;
+            const searchByTime = this.props.searchBy.availability !== "Available Now"?
+                <section>
+                    <label>Date
+                        <DatePicker defaultValue={this.props.searchBy.isoValue} {...this.props}
+                                    action={this.props.actions.searchByActions.changeInviteDate}
+                                    changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
+                    </label>
+                    <label>Start Time
+                        <TimeInput defaultValue={this.props.searchBy.fromTime} {...this.props}
+                                   action={this.props.actions.searchByActions.changeInviteFromTime}
+                                   changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
+                    </label>
+                    <label>End Time
+                        <TimeInput defaultValue={this.props.searchBy.toTime} {...this.props}
+                                   action={this.props.actions.searchByActions.changeInviteToTime}
+                                   changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
+                    </label>
+                </section>: null;
+
             if (view === "location") {
                 navView = <div className="google-map" style={{width: '100%', height: '240px'}}>
                     <GoogleMaps sitters={this.props.feed.matches}/></div>;
             }
             //<GoogleMaps center={{lat: this.props.user.address? this.props.user.address.latitude: 0,lng: this.props.user.address? this.props.user.address.longitude: 0}}*/}
-                        ///*sitter={this.props.user}*/}
-                        ///*oneMarker={true}/>*/}  // FIX THIS
+            ///*sitter={this.props.user}*/}
+            ///*oneMarker={true}/>*/}  // FIX THIS
             else if (view === "time") {
+
                 navView = <form id="time-search">
                     <RadioGroup options={strings.AVAILABILITY}
-                                defaultValue={this.props.user.partnerGender ?  this.props.user.partnerGender[0].toUpperCase() + this.props.user.partnerGender.slice(1):"" }
+                                defaultValue={this.props.searchBy.availability}
                                 action={this.props.actions.searchByActions.changeAvailability}
                                 radioType={'availability'}
-                                value={this.props.user.gender}/>
-                    <label>Date
-                    <DatePicker defaultValue={this.props.searchBy.isoValue} {...this.props}
-                                action={this.props.actions.searchByActions.changeInviteDate}
-                                changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
-                    </label>
-                    <label>Start Time
-                    <TimeInput defaultValue={this.props.searchBy.fromTime} {...this.props}
-                               action={this.props.actions.searchByActions.changeInviteFromTime}
-                               changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
-                    </label>
-                    <label>End Time
-                    <TimeInput defaultValue={this.props.searchBy.toTime} {...this.props}
-                               action={this.props.actions.searchByActions.changeInviteToTime}
-                               changeSitters={this.props.actions.feedActions.setFilteredMatches}/>
-                    </label>
+                                value={this.props.searchBy.availability}/>
+                    {searchByTime}
                 </form>;
             }
             else if (view === "rate") {
                 navView = <div id="range-search">
                     <label>Hour Rate
-                    <Range min={0} max={50} {...this.props} action={this.props.actions.rangeActions.changeRange}
-                           changeSitters={this.props.actions.feedActions.setFilteredMatches} disabled={false}/>
+                        <Range min={0} max={50} {...this.props} action={this.props.actions.rangeActions.changeRange}
+                               changeSitters={this.props.actions.feedActions.setFilteredMatches} disabled={false}/>
                     </label>
                 </div>
             }
