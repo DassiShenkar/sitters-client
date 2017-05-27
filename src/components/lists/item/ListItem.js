9c817db7437b82b51256aef273eb436caefe41b5
DassiShenkar <dassi.shenkar@gmail.com>
2017-05-27 16:14:35 +0300

    rearanged item

---
+++ src/components/lists/item/ListItem.js
@@ -0,0 +1,26 @@
+// external sources
+import React from 'react';
+
+// components
+import {Image} from 'react-bootstrap';
+import {Link} from 'react-router';
+
+class ListItem extends React.Component {
+    render() {
+        let item = this.props.item;
+        return (
+            <li className="item">
+                <Link className="item-link" to={'/' + this.props.type + '/' + item._id}>
+                    <Image src={this.props.isParent ? item.sitterImage : item.parentImage}
+                           alt={this.props.isParent ? item.sitterName : item.parentName} circle/>
+                    <div className="item-info">
+                        <p><strong>{this.props.isParent ? item.sitterName : item.parentName}</strong></p>
+                        <p><span className={'icon-circle ' + item.status}/>{item.status + ' ' + item.date}</p>
+                    </div>
+                </Link>
+            </li>
+        );
+    }
+}
+
+export default ListItem;
