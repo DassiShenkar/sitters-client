43811ad5685f26a39618d38532bd8478e26ba2ed
DassiShenkar <dassi.shenkar@gmail.com>
2017-05-27 19:30:47 +0300

    enable review button only if you've invited this sitter

--- src/components/panels/actionPanel/index.js
+++ src/components/panels/actionPanel/index.js
@@ -4,6 +4,7 @@ import React from "react";
 // style
 import "./style.css";
 import Invite from "../../invite/Invite";
+import * as _ from "lodash";
 
 class SitterActionBar extends React.Component {
 
@@ -12,12 +13,12 @@ class SitterActionBar extends React.Component {
         this.inviteSitter = this.inviteSitter.bind(this);
         this.reviewSitter = this.reviewSitter.bind(this);
         this.sitterProfile = this.sitterProfile.bind(this);
+        this.shouldReview = this.shouldReview.bind(this);
     }
 
     inviteSitter(e) {
         e.preventDefault();
-        let sitter;
-        sitter = this.props.feed.filteredMatches[this.props.feed.sitterIndex];
+        const sitter = this.props.feed.filteredMatches[this.props.feed.sitterIndex];
         this.props.actions.sitterProfileActions.setSitter(sitter);
         this.props.actions.feedActions.showInvitePopup(true);
     }
@@ -32,12 +33,18 @@ class SitterActionBar extends React.Component {
         this.props.router.push('/sitter/' + this.props.feed.filteredMatches[this.props.feed.sitterIndex]._id);
     }
 
+    shouldReview() {
+        return this.props.user.invites.find(invite => {
+            return invite.sitterID === this.props.feed.matches[this.props.feed.sitterIndex]._id;
+        })
+    }
+
     render() {
         return (
             <div id="sitterActionBar">
                 <button onClick={this.inviteSitter}><span className="icon-envelope action-icon"/></button>
                 <button className={this.props.router.getCurrentLocation().pathname.includes('sitter') ? 'selected' : 'unselected'} onClick={this.sitterProfile}><span className='icon-user'/></button>
-                <button onClick={this.reviewSitter}><span className="icon-heart"/></button>
+                <button className={this.shouldReview() ? 'enabled': 'disabled'} onClick={this.shouldReview() ? this.reviewSitter : _.noop()}><span className='icon-heart'/></button>
                 <Invite {...this.props}/>
             </div>
         )
