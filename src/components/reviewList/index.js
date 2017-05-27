fd5171444fb767aa054c93bd29ce377d5864d2b1
DassiShenkar <dassi.shenkar@gmail.com>
2017-05-27 20:18:58 +0300

    removed header from review list

--- src/components/reviewList/index.js
+++ src/components/reviewList/index.js
@@ -5,7 +5,6 @@ class ReviewList extends React.Component {
     render() {
         return (
             <div className="review-list">
-                <h3>Reviews</h3>
                 {this.props.sitterProfile.sitter.reviews.map((review, index) => <ReviewItem {...this.props} key={index} index={index} review={review}/>)}
             </div>
         )
