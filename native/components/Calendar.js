3b520e4763f3668154ec895235ebf1708c18ee43
Gregory <kogrego@gmail.com>
2017-05-28 16:49:27 +0300

    added calendar

---
+++ native/components/Calendar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, AsyncStorage, StyleSheet } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+class Calendar extends React.component {
+    
+    constructor(props) {
+        super(props);
+    }
+
+    
+    render() {
+        return (
+            <View></View>
+        );
+    }
+}
+
+
+export default Calendar;
