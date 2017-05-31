a7c6266e1dd13c51f45eb4db8fb805e789ed7394
DassiShenkar <dassi.shenkar@gmail.com>
2017-05-31 14:46:03 +0300

    removed median and added mobility

--- src/components/pages/profile/index.js
+++ src/components/pages/profile/index.js
@@ -2,11 +2,10 @@
 import React from "react";
 import geodist from "geodist";
 import axios from "axios";
-
-import {Image, Table, Panel, Accordion, ControlLabel} from "react-bootstrap";
-
+import * as _ from "lodash";
 
 // components
+import {Image, Table, Panel, Accordion, ControlLabel} from "react-bootstrap";
 import SitterActionBar from "../../panels/actionPanel";
 import ReviewList from "../../reviewList/index";
 import Invite from "../../invite/Invite";
@@ -19,48 +18,6 @@ import MatchBanner from "../../banners/matchBanner";
 import './style.css';
 
 class SitterProfile extends React.Component {
-
-    median(values) {
-
-        values.sort(function (a, b) {
-            return a - b;
-        });
-
-        var half = Math.floor(values.length / 2);
-
-        if (values.length % 2)
-            return values[half];
-        else
-            return (values[half - 1] + values[half]) / 2.0;
-    }
-
-    componentDidMount() {
-        if (strings.ACTIVATE_MEDIAN) {
-            let parent = this.props.user;
-            const id = this.props.params.sitterId;
-            parent.matchBI.matchScores.push(this.props.feed.matches.find(function (sitter) {
-                return sitter._id === id;
-            }).matchScore);
-            parent.matchBI.median = this.median(parent.matchBI.matchScores);
-            axios({
-                method: 'post',
-                url: (strings.DEBUG ? strings.LOCALHOST : strings.WEBSITE ) + 'parent/update',
-                headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
-                data: parent
-            }).then(function (res) {
-                if (res.data) {  // user created
-                }
-                else {
-                    console.log("parentBI not updated");
-                    //TODO: think about error
-                }
-            })
-                .catch(function (error) {
-                    alert(error);
-                    //TODO: think about error
-                });
-        }
-    }
 
     componentWillMount() {
         let self = this;
@@ -85,17 +42,6 @@ class SitterProfile extends React.Component {
             });
     }
 
-    handleChangeReview(e) {
-        this.props.actions.sitterProfileActions.setReviewDescription(e.target.value);
-    }
-
-    inviteSitter(e) {
-        e.preventDefault();
-        let sitter = this.props.feed.filteredMatches[this.props.feed.sitterIndex];
-        this.props.actions.sitterProfileActions.setSitter(sitter);
-        this.props.actions.feedActions.showInvitePopup(true);
-    }
-
     addReview() {
         this.props.actions.feedActions.showReviewPopup(true);
     }
@@ -107,23 +53,25 @@ class SitterProfile extends React.Component {
     render() {
         let self = this;
         const id = this.props.params.sitterId;
-        const workingHours = Object.keys(this.props.sitterProfile.sitter.workingHours).map(function (key, index) {
+        const workingHours = Object.keys(this.props.sitterProfile.sitter.workingHours).map(function (day, index) {
             return (
                 <tr key={index}>
-                    <td>{key[0].toUpperCase() + key.slice(1)}</td>
-                    <td>{self.props.sitterProfile.sitter.workingHours[key].start + '-' + self.props.sitterProfile.sitter.workingHours[key].finish}</td>
+                    <td>{day[0].toUpperCase() + day.slice(1)}</td>
+                    <td>{_.join(self.props.sitterProfile.sitter.workingHours[day], ', ')}</td>
                 </tr>
             )
         });
-        let hobbies = null, education = null, languages = null, expertise = null;
+        let hobbies = null, education = null, languages = null, expertise = null, mobility = null;
         if (this.props.sitterProfile.sitter.hobbies.length > 0)
-            hobbies = <AccordionPanel header="+ HOBBIES" list={this.props.sitterProfile.sitter.hobbies}/>;
+            hobbies = <AccordionPanel header="HOBBIES" list={this.props.sitterProfile.sitter.hobbies}/>;
+        if (this.props.sitterProfile.sitter.mobility.length > 0)
+            mobility = <AccordionPanel header="MOBILITY" list={this.props.sitterProfile.sitter.mobility}/>;
         if (this.props.sitterProfile.sitter.languages.length > 0)
-            languages = <AccordionPanel header="+ LANGUAGES" list={this.props.sitterProfile.sitter.languages}/>;
+            languages = <AccordionPanel header="LANGUAGES" list={this.props.sitterProfile.sitter.languages}/>;
         if (this.props.sitterProfile.sitter.education.length > 0)
-            education = <AccordionPanel header="+ EDUCATION" list={this.props.sitterProfile.sitter.education}/>;
+            education = <AccordionPanel header="EDUCATION" list={this.props.sitterProfile.sitter.education}/>;
         if (this.props.sitterProfile.sitter.expertise.length > 0)
-            expertise = <AccordionPanel header="+ EXPERTISE" list={this.props.sitterProfile.sitter.expertise}/>;
+            expertise = <AccordionPanel header="EXPERTISE" list={this.props.sitterProfile.sitter.expertise}/>;
         const coverPhoto = this.props.sitterProfile.sitter.coverPhoto ? this.props.sitterProfile.sitter.coverPhoto : '';
         const lastInvite = this.props.sitterProfile.sitter ?
             <div className="last-invited">
@@ -171,7 +119,7 @@ class SitterProfile extends React.Component {
                 <div className="sitter-details page">
                     {lastInvite}
                     <Accordion>
-                        <Panel header="+ AVAILABILITY" eventKey="availability">
+                        <Panel header="AVAILABILITY" eventKey="availability">
                             <Table id="availability-table" responsive>
                                 <thead>
                                 <tr>
@@ -185,16 +133,14 @@ class SitterProfile extends React.Component {
                             </Table>
                         </Panel>
                     </Accordion>
+                    {mobility}
                     {education}
                     {languages}
                     {hobbies}
                     {expertise}
-                    {/*<ControlLabel>Address</ControlLabel>*/}
-                    {/*<p>{sitterAddress}</p>*/}
-
                     {this.props.sitterProfile.sitter.reviews.length > 0 ?
                         <Accordion>
-                            <Panel header="+ REVIEWS" eventKey="reviews">
+                            <Panel header="REVIEWS" eventKey="reviews">
                                 <ReviewList reviews={this.props.sitterProfile.sitter.reviews} {...this.props}/>
                             </Panel>
                         </Accordion> : ''}
