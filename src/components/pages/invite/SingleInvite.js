bc06e68ee13412853dc21e000e29f5d77cbbade1
DassiShenkar <dassi.shenkar@gmail.com>
2017-05-31 19:04:48 +0300

    changed invite logic

--- src/components/pages/invite/SingleInvite.js
+++ src/components/pages/invite/SingleInvite.js
@@ -3,22 +3,22 @@ import {ControlLabel, Image} from "react-bootstrap";
 import GoogleMaps from "../../controllers/maps/GoogleMaps";
 import axios from 'axios';
 import strings from "../../../static/strings";
+import * as _ from "lodash";
 
 export default class SingleInvite extends React.Component{
 
     componentWillMount(){
         const inviteID = this.props.router.params.inviteId;
         let user = this.props.user;
-        let shouldUpdate = true;
-        user.invites.forEach(function(invite){
-            if(invite._id === inviteID && !invite.wasRead){
-                invite.wasRead = true;
-            }
-            else
-                shouldUpdate = false;
-        });
+        const  inviteIndex = _.findIndex(user.invites, function(o) { return o._id === inviteID; });
+
+        const shouldUpdate = !!((user.isParent && user.invites[inviteIndex].status !== "waiting" && !user.invites[inviteIndex].wasRead)
+        || (!user.isParent && user.invites[inviteIndex].status === "waiting" && !user.invites[inviteIndex].wasRead));
+
         if(shouldUpdate){
-            const path = this.props.user.isParent? 'parent/update': 'sitter/update';
+            user.invites[inviteIndex].wasRead = true;
+            this.props.actions.inviteActions.setInvites(user.invites);
+            const path = this.props.user.isParent ? 'parent/update': 'sitter/update';
             axios({
                 method: 'post',
                 url: (strings.DEBUG?strings.LOCALHOST : strings.WEBSITE ) + path,
@@ -58,9 +58,13 @@ export default class SingleInvite extends React.Component{
                     <ControlLabel>Watch Place:</ControlLabel>
                     {this.props.user.address ? "   " + this.props.user.address.street + " " + this.props.user.address.houseNumber + ", " +this.props.user.address.city : ''}
                     <div style={{width: '100%', height: '400px'}}>
-                        <GoogleMaps {...this.props} center={{lat: this.props.user.address? this.props.user.address.latitude: 0,lng: this.props.user.address? this.props.user.address.longitude: 0}}
-                                    sitter={this.props.user}
-                                    oneMarker={true}/>
+                        <div style={{width: '100%', height: '400px'}}>
+                            <GoogleMaps center={{lat: this.props.user.address? this.props.user.address.latitude: 0,lng: this.props.user.address? this.props.user.address.longitude: 0}}
+                                        sitters={[this.props.user]}
+                                        oneMarker={true}
+                                        zoom="14"
+                            />
+                        </div>
                     </div>
                     <ControlLabel>Notes</ControlLabel>
                     <p>{invite.notes}</p>
