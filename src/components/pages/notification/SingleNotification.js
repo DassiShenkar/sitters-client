8c1fd156f7e7f8dd6f766ff6cafd14da8761c824
DassiShenkar <dassi.shenkar@gmail.com>
2017-06-01 17:50:23 +0300

    fixed notification single page bug

--- src/components/pages/notification/SingleNotification.js
+++ src/components/pages/notification/SingleNotification.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import {ControlLabel, Image} from "react-bootstrap";
-import GoogleMaps from "../../controllers/maps/GoogleMaps";
+import {ControlLabel, Image, PageHeader} from "react-bootstrap";
+import MatchBanner from "../../banners/matchBanner";
 import strings from "../../../static/strings";
 import axios from 'axios';
+import * as _ from "lodash";
+import './style.css';
 
-export default class SingleNotification extends React.Component{
+export default class SingleNotification extends React.Component {
 
-    componentWillMount(){
+    componentWillMount() {
         const notificationId = this.props.router.params.notificationId;
         let user = this.props.user;
         let shouldUpdate = true;
-        user.notifications.forEach(function(notification){
-            if(notification._id === notificationId){
-                if(notification.wasRead){
+        user.notifications.forEach(function (notification) {
+            if (notification._id === notificationId) {
+                if (notification.wasRead) {
                     shouldUpdate = false;
                 }
                 notification.wasRead = true;
             }
         });
-        if(shouldUpdate){
-            const path = this.props.user.isParent? 'parent/update': 'sitter/update';
+        if (shouldUpdate) {
+            const path = this.props.user.isParent ? 'parent/update' : 'sitter/update';
             axios({
                 method: 'post',
-                url: (strings.DEBUG?strings.LOCALHOST : strings.WEBSITE ) + path,
+                url: (strings.DEBUG ? strings.LOCALHOST : strings.WEBSITE ) + path,
                 headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
                 data: user
             }).then(function (res) {
@@ -45,27 +47,27 @@ export default class SingleNotification extends React.Component{
     render() {
         const notificationID = this.props.router.params.notificationId;
         const notification = this.props.user.notifications.filter((notification) => notification._id === notificationID)[0];
+        const sitter = _.find(this.props.feed.matches, (sitter) => {
+            return sitter._id === notification.sitterID;
+        });
+        const style = {
+            backgroundImage: 'url(' + sitter.coverPhoto + ')',
+            height: '400px'
+        };
         return (
-            <div>
-                <Image className="sitter-image" src={notification.sitterPicture} alt={notification.sitterName} circle={true}/>
-                <h4 className="sitter-name">{notification.sitterName}</h4>
-                <form id="invite">
-                    <ControlLabel>Date</ControlLabel>
-                    <p>{notification.date}</p>
-                    <ControlLabel>Start Watch</ControlLabel>
-                    <p>{notification.startTime}</p>
-                    <ControlLabel>End Watch</ControlLabel>
-                    <p>{notification.endTime}</p>
-                    <ControlLabel>Watch Place:</ControlLabel>
-                    {this.props.user.address ? "   " + this.props.user.address.street + " " + this.props.user.address.houseNumber + ", " +this.props.user.address.city : ''}
-                    <div style={{width: '100%', height: '400px'}}>
-                        <GoogleMaps center={{lat: this.props.user.address? this.props.user.address.latitude: 0,lng: this.props.user.address? this.props.user.address.longitude: 0}}
-                                    sitter={this.props.user}
-                                    oneMarker={true}/>
+            <div id="single-notification-page" className="page">
+                <PageHeader>Meet The New Sitter - {notification.sitterName}</PageHeader>
+                <div className="match" style={style}>
+                    <div className="cover-overlay"/>
+                    <div className="sitter-info">
+                        <Image className="profilePic"
+                               src={sitter.profilePicture}
+                               alt={sitter.name}
+                               circle/>
+                        <h1 className="sitterName">{sitter.name}</h1>
                     </div>
-                    <ControlLabel>Notes</ControlLabel>
-                    <p>{notification.notes}</p>
-                </form>
+                </div>
+                <MatchBanner parent={this.props.user} sitter={sitter} matchScore={sitter.match}/>
             </div>
         )
     }
