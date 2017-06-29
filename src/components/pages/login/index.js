9eb14a06e4b37dabdd7e632e8d645b3dd834378a
DassiShenkar <dassi.shenkar@gmail.com>
2017-06-29 14:05:12 +0300

    added bg video to login page

--- src/components/pages/login/index.js
+++ src/components/pages/login/index.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 
 //components
 import FacebookLogin from 'react-facebook-login';
-import PageHeader from 'react-bootstrap/lib/PageHeader';
 import Form from 'react-bootstrap/lib/Form';
+import ReactPlayer from 'react-player';
 
 import RadioGroup from '../../controllers/radio/radioGroup/index';
 
@@ -78,22 +78,25 @@ class Login extends React.Component {
                                           value={ this.props.user.userType }
                                           required={true}/>;  //TODO: do not delete - for beta
         return (
-            <div id="login-page" className="page">
-                <header>
-                    <h1>{strings.APP_NAME}</h1>
-                    <h3 className="tagline">{strings.APP_DESCRIPTION}</h3>
-                </header>
-                <Form className="login-form" horizontal>
-                    {document.cookie.replace(/(?:(?:^|.*;\s*)auth_token\s*=\s*([^;]*).*$)|^.*$/, "$1") !== '' ? '' : userTypeRadio}
-                    <FacebookLogin
-                        appId="268453370262293"
-                        autoLoad={false}
-                        fields="id,name,email,cover,birthday,currency,education,gender,friends,friendlists,languages,location,timezone,picture.width(100).height(100)"
-                        scope="user_birthday,public_profile,user_location,user_education_history,user_likes,email,user_friends"
-                        callback={this.login}
-                        {...this.props}
-                    />
-                </Form>
+            <div id="login-page">
+                <ReactPlayer url='./Shoes.mp4' playing loop controls={false} width={"auto"} height={"auto"} style={{"width": "auto", "height": "auto"}}/>
+                <div id="login-container" className="page">
+                    <header>
+                        <h1>{strings.APP_NAME}</h1>
+                        <h3 className="tagline">{strings.APP_DESCRIPTION}</h3>
+                    </header>
+                    <Form id="login-form" horizontal>
+                        {document.cookie.replace(/(?:(?:^|.*;\s*)auth_token\s*=\s*([^;]*).*$)|^.*$/, "$1") !== '' ? '' : userTypeRadio}
+                        <FacebookLogin
+                            appId="268453370262293"
+                            autoLoad={false}
+                            fields="id,name,email,cover,birthday,currency,education,gender,friends,friendlists,languages,location,timezone,picture.width(100).height(100)"
+                            scope="user_birthday,public_profile,user_location,user_education_history,user_likes,email,user_friends"
+                            callback={this.login}
+                            {...this.props}
+                        />
+                    </Form>
+                </div>
             </div>
         )
     }
