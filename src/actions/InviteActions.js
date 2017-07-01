3f79841d1cc5f94493020402cb904766e31dea64
DassiShenkar <dassi.shenkar@gmail.com>
2017-07-01 17:43:57 +0300

    removed add invite action

--- src/actions/InviteActions.js
+++ src/actions/InviteActions.js
@@ -1,13 +1,3 @@
-export function sendInvite(date, time, location, comments) {
-    return {
-        type: 'ADD_INVITE',
-        date,
-        time,
-        location,
-        comments
-    }
-}
-
 export function updateDate(date) {
     return {
         type: 'UPDATE_DATE',
